664b898e6617d32bf8c60342e40fba243003ddc6
AlexWayke <yuhcha2@yandex.ru>
2022-10-23 01:14:30 +0400

    Script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-const accordionBtn = document.querySelectorAll(".accordion");
-console.log(accordionBtn);
+const accordionBtn = document.querySelectorAll('.accordion');
+const accordionContainer = document.querySelectorAll('.accordion-container');
+
+console.log(accordionContainer)
+const openAccordion = function(accordionContainer){
+
+}
+
+accordionBtn.forEach(e => e.addEventListener("click", openAccordion));
